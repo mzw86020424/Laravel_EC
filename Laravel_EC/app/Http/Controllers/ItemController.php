372d6f15d93d7cb884dc7c744c4d6cdcc1b13954
mzw86020424 <mzw86020424@gmail.com>
2021-04-04 15:20:14 +0900

    商品検索機能を追加

--- Laravel_EC/app/Http/Controllers/ItemController.php
+++ Laravel_EC/app/Http/Controllers/ItemController.php
@@ -12,9 +12,13 @@ class ItemController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
-    {
-        $items = Item::paginate(15);
+    public function index(Request $request)
+    {   
+        if($request->has('keyword')) {
+            $items = Item::where('name', 'like', '%'.$request->get('keyword').'%')->paginate(15);
+        } else {   
+            $items = Item::paginate(15);
+        }
         return view('item/index', ['items' => $items]);
     }
 
